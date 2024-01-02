19e01a45ea3af0bfce1025d55de778c98ddc5dfc
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-01-02 20:59:15 +0000

    fix: fixed the security issue  + used concurrently to run app.

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #starts the server up
 #run chmod +x script.sh && ./script.sh
-cd backend && npm install && node . 
+#cd backend && npm install && node . 
+
+#Use concurrently instead
+
+npx concurrently "cd backend && npm install && node ." "cd frontend && open index.html"
+
